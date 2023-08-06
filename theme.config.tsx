fd07192812f9d0b1ef234e4a3c55fcf6de44539c
AlexiaHyn <sunfloweralexia@hotmail.com>
2023-08-05 21:55:25 -0500

    update logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: 
-  <div style={{display: "flex", fontWeight: "bold"}}>
-    <img width={120} height={50} src="./components/logo.png" placeholder='Owlracle Logo'/>
-    Owlracle 
+  <div style={{display: "flex", alignItems: "center", fontWeight: "bold", backgroundColor: "white", borderRadius: "5px", padding: "5px"}}>
+    <img width={50} src="/logo.png" placeholder='Owlracle Logo' />
+    <div style={{color: "black", marginLeft: "5px"}}>Owlracle</div>
   </div>,
   project: {
     link: 'https://github.com/Mr-Ye-Cao/Owlracle.git',
