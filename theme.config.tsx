5b1c0e1dc6f849dfb17b2e77caa2196a7c9944a7
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-08-06 10:20:46 -0500

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,27 +2,31 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <img width={120} src="https://docs.sweep.dev/banner.png" style={{borderRadius: 5}}/>,
+  logo: 
+  <div style={{display: "flex", alignItems: "center", fontWeight: "bold", backgroundColor: "white", borderRadius: "5px", padding: "5px"}}>
+    <img width={50} src="/logo.png" placeholder='Owlracle Logo' />
+    <div style={{color: "black", marginLeft: "5px"}}>Owlracle</div>
+  </div>,
   head: (
     <head>
       <link rel="icon" type="image/png" href="./favicon.png" />
-      <meta property="og:title" content="Sweep AI Documentation" />
-      <meta property="og:description" content="The official documentation for Sweep AI." />
-      <meta property="og:image" content="https://docs.sweep.dev/banner.png" />
+      <meta property="og:title" content="Owlracle Documentation" />
+      <meta property="og:description" content="The official documentation for Owlracle." />
+      <meta property="og:image" content="https://owlracle-documentation-deploy.vercel.app/banner.png" />
       <script defer src="/_vercel/insights/script.js"></script>
     </head>
   ),
   project: {
-    link: 'https://github.com/sweepai/sweep',
+    link: 'https://github.com/Open-Nice/Owlracle',
   },
   chat: {
-    link: 'https://discord.gg/sweep-ai',
+    link: 'https://discord.gg/WSBbXqPh',
   },
-  docsRepositoryBase: 'https://github.com/sweepai/sweep-docs',
-  darkMode: true,
+  docsRepositoryBase: 'https://github.com/Open-Nice/Owlracle',
+  darkMode: false,
   primaryHue: 220,
   footer: {
-    text: 'Sweep AI © 2023',
+    text: 'Owlracle © 2023',
   },
   useNextSeoProps() {
     return {
